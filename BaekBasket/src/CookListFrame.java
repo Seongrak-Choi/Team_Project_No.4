5d00e45bfb3932be19877fcc932cde9ae86442ed
injong8461 <71006153+injong8461@users.noreply.github.com>
2020-11-15 19:08:32 +0900

    CookListFrame_Final_Update

--- BaekBasket/src/CookListFrame.java
+++ BaekBasket/src/CookListFrame.java
@@ -1,103 +1,87 @@
+package Team;
+
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class CookListFrame extends JFrame {
-	JPanel[] Jpanel = new JPanel[7]; // 패널, 리스트, 스크롤 생성
-	JList[] Jlist = new JList[7];
-	JScrollPane[] Jsb = new JScrollPane[7];
-	JFrame frame = new JFrame("요리 리스트 화면"); 
+	int index=MainFrame.index;
+	JFrame frame = new JFrame("요리 리스트 화면");
 	JPanel pFrame = new JPanel();
-	JButton bNightmeal = new JButton("야식&안주");
 	JButton bSnack = new JButton("간식");
-	JButton bKorean = new JButton("한식");
-	JButton bChinese = new JButton("중식");
-	JButton bWestern = new JButton("양식");
+	JButton bSoup = new JButton("찌개&국");
 	JButton bSide = new JButton("반찬");
-	JButton bCanCook = new JButton("만들 수 있는 요리 확인");
-	JButton bBarsket = new JButton("장바구니");
+	JButton bRice = new JButton("밥");
+	JButton bEtc = new JButton("기타");
 	JButton bBack = new JButton("이전");
 	JTextField tSearch = new JTextField();
 	JButton bSearch = new JButton("검색");
+	JPanel pCookList = new JPanel();
+	String[] snack = new String[MainFrame.snack];
+	String[] soup = new String[MainFrame.soup];
+	String[] side = new String[MainFrame.side];
+	String[] rice = new String[MainFrame.rice];
+	String[] etc =new String[MainFrame.etc];
+	String[] cook = new String[index];
+	JList lCookList = new JList(cook);
+	String[] compare = new String[index];
 
-	public CookListFrame() {
-		frame.setLocation(0,0);
-		frame.setSize(800,600);
+	public CookListFrame(Food[] food) {
+		Food[] f= food;
+		frame.setLocation(0, 0); // 필요한 컴포넌트들 추가
+		frame.setSize(800, 600);
 		frame.setLayout(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		String[] allMenu = new String[5];
-		allMenu[0]="야식0";
-		allMenu[1]="간식0";
-		allMenu[2]="한식0";
-		allMenu[3]="오태식0";
-		allMenu[4]="육식0";
-		String[] compare = new String[allMenu.length];
-
-		for(int i=0;i<allMenu.length;i++) // compare에 allMenu복사
-			compare[i]=allMenu[i];
-
-		pFrame.setLocation(0,0);
-		pFrame.setSize(800,600);
+		pFrame.setLocation(0, 0);
+		pFrame.setSize(800, 600);
 		pFrame.setLayout(null);
 		pFrame.setBackground(Color.WHITE);
 
-		bNightmeal.setLocation(50,20);
-		bNightmeal.setSize(100,30);
-		pFrame.add(bNightmeal);
-		bNightmeal.setBackground(Color.WHITE);
-
-		bSnack.setLocation(150,20);
-		bSnack.setSize(75,30);
+		bSnack.setLocation(50, 20);
+		bSnack.setSize(100, 30);
 		pFrame.add(bSnack);
 		bSnack.setBackground(Color.WHITE);
 
-		bKorean.setLocation(225,20);
-		bKorean.setSize(75,30);
-		pFrame.add(bKorean);
-		bKorean.setBackground(Color.WHITE);
+		bSoup.setLocation(150, 20);
+		bSoup.setSize(90, 30);
+		pFrame.add(bSoup);
+		bSoup.setBackground(Color.WHITE);
 
-		bChinese.setLocation(300,20);
-		bChinese.setSize(75,30);
-		pFrame.add(bChinese);
-		bChinese.setBackground(Color.WHITE);
-
-		bWestern.setLocation(375,20);
-		bWestern.setSize(75,30);
-		pFrame.add(bWestern);
-		bWestern.setBackground(Color.WHITE);
-
-		bSide.setLocation(450,20);
-		bSide.setSize(75,30);
+		bSide.setLocation(240, 20);
+		bSide.setSize(75, 30);
 		pFrame.add(bSide);
 		bSide.setBackground(Color.WHITE);
 
-		bCanCook.setLocation(380,520);
-		bCanCook.setSize(170,30);
-		pFrame.add(bCanCook);
-		bCanCook.setBackground(Color.WHITE);
+		bRice.setLocation(315, 20);
+		bRice.setSize(75, 30);
+		pFrame.add(bRice);
+		bRice.setBackground(Color.WHITE);
 
-		bBarsket.setLocation(550,520);
-		bBarsket.setSize(90,30);
-		pFrame.add(bBarsket);
-		bBarsket.setBackground(Color.WHITE);
+		bEtc.setLocation(390, 20);
+		bEtc.setSize(75, 30);
+		pFrame.add(bEtc);
+		bEtc.setBackground(Color.WHITE);
 
-		bBack.setLocation(640,520);
-		bBack.setSize(75,30);
+		bBack.setLocation(690, 520);
+		bBack.setSize(75, 30);
 		pFrame.add(bBack);
-		bBack.setBackground(Color.WHITE);
+		bBack.setBackground(Color.WHITE);		
 
-		tSearch.setLocation(550,20);
+		tSearch.setLocation(550, 20);
 		tSearch.setSize(150,30);
 		pFrame.add(tSearch);
 		tSearch.setBorder(new LineBorder(Color.BLACK));
@@ -107,128 +91,113 @@ public class CookListFrame extends JFrame {
 		pFrame.add(bSearch);
 		bSearch.setBackground(Color.WHITE);
 
-		String[] nightmeal = new String[30]; // 야식 넣을 칸
-		for(int i=0;i<30;i++)
-			nightmeal[i] = "야식"+i ;
+		pCookList.setLocation(50, 75);
+		pCookList.setSize(665, 425);
+		pCookList.setLayout(null);
+		pCookList.setVisible(true);
 
-		String[] snack = new String[30]; // 간식
-		for(int i=0;i<30;i++)
-			snack[i] = "간식"+i ;
-
-		String[] korean = new String[30]; // 한식
-		for(int i=0;i<30;i++)
-			korean[i] = "한식"+i ;
-
-		for(int i=0;i<7;i++) {
-			Jpanel[i] = new JPanel();
-			Jpanel[i].setLocation(50,75);
-			Jpanel[i].setSize(665,425);
-			Jpanel[i].setLayout(null);
-			if(i!=0)
-				Jpanel[i].setVisible(false);
-
-			if(i==0) 
-				Jlist[i] = new JList(nightmeal);
-			else if(i==1)
-				Jlist[i] = new JList(snack);
-			else if(i==2)
-				Jlist[i] = new JList(korean);
-			else if(i==3)
-				Jlist[i] = new JList();
-			else if(i==4)
-				Jlist[i] = new JList();
-			else if(i==5)
-				Jlist[i] = new JList();
-			else if(i==6)
-				Jlist[i] = new JList(allMenu);
-
-			Jlist[i].setLocation(0,0);
-			Jlist[i].setSize(665,425);
-			Jlist[i].setBorder(new LineBorder(Color.BLACK));
-
-			Jsb[i] = new JScrollPane(Jlist[i]);
-			Jsb[i].setLocation(0,0);
-			Jsb[i].setSize(665,425);
-			Jpanel[i].add(Jsb[i]);
-			frame.add(Jpanel[i]);
-		}
-		bNightmeal.addActionListener(new EventHandler());
-		bSnack.addActionListener(new EventHandler());
-		bKorean.addActionListener(new EventHandler());
-		bBack.addActionListener(new EventHandler());
-
-		bSearch.addActionListener(new ActionListener(){  // 각버튼 리스너
-			public void actionPerformed(ActionEvent e){
-				if(e.getSource()==bSearch) {
-					String searchCook = tSearch.getText(); 
-
-					for(int i=0;i<compare.length;i++)
-						allMenu[i]=compare[i];
-					for(int i=0;i<compare.length;i++) {
-						if(allMenu[i].equals(searchCook)) {
-							allMenu[0] = allMenu[i];
-						}
-					}
-
-					for(int i=1;i<compare.length;i++) {
-						if(allMenu[0].contentEquals(searchCook))
-							allMenu[i]=null;
-					}
-
-					for(int i=0;i<7;i++) {
-						if(i!=6)
-							Jpanel[i].setVisible(false);
-						else
-							Jpanel[i].setVisible(true);
-					}
-				}
-				else if(e.getSource()==bKorean) {
-					for(int i=0;i<7;i++) {
-						if(i!=2)
-							Jpanel[i].setVisible(false);
-						else
-							Jpanel[i].setVisible(true);
-					}
-				}
+		int iSnack =0; // 순서대로 적재하기 위한 필드 선언
+		int iSoup =0;
+		int iSide =0;
+		int iRice =0;
+		int iEtc =0;
+		for(int i=0;i<index;i++) { // 분류 된 메뉴를 순서대로 저장
+			if(f[i].categoryNo==0) {
+				snack[iSnack]=f[i].name;
+				iSnack++;
 			}
-		});;
+			else if(f[i].categoryNo==1) {
+				soup[iSoup]=f[i].name;
+				iSoup++;
+			}
+			else if(f[i].categoryNo==2) {
+				side[iSide]=f[i].name;
+				iSide++;
+			}
+			else if(f[i].categoryNo==3) {
+				rice[iRice]=f[i].name;
+				iRice++;
+			}
+			else if(f[i].categoryNo==4) {
+				etc[iEtc]=f[i].name;
+				iEtc++;
+			}
+		}
+		for(int i=0;i<index;i++) { // cook배열 초기화
+			cook[i] = f[i].name;
+		}
 
+		for(int i=0;i<index;i++)
+			compare[i]=cook[i];
+
+		lCookList.setLocation(0, 0);
+		lCookList.setSize(665, 425);
+		lCookList.setBorder(new LineBorder(Color.BLACK));
+
+		lCookList.addMouseListener(new MouseAdapter() { // 리스트 배열 더블클릭 이벥트 
+			@Override public void mouseClicked(MouseEvent e) {
+				if(e.getClickCount()==2) {
+					String compare=(String)lCookList.getSelectedValue(); 
+					int j=0;
+					for(int i=0;i<index;i++) {
+						if(f[i].name.equals(compare))
+							j=i;
+					}	
+					RecipeInterface recipeInterface = new RecipeInterface(f[j]); // 선택한 음식을 넘기며 RecipeInterface 생성
+				}
+			}});
+
+		JScrollPane sb = new JScrollPane(lCookList);
+		sb.setLocation(0,0);
+		sb.setSize(665,425);
+		pCookList.add(sb);
+
+		bSnack.addActionListener(new EventHandler());
+		bSoup.addActionListener(new EventHandler());
+		bSide.addActionListener(new EventHandler());
+		bRice.addActionListener(new EventHandler());
+		bEtc.addActionListener(new EventHandler());
+		bBack.addActionListener(new EventHandler());
+		bSearch.addActionListener(new EventHandler());
+
+		frame.add(pCookList);
 		frame.add(pFrame);
+
 		frame.setVisible(true);
 	}
 	class EventHandler implements ActionListener{
-
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if(e.getSource()==bNightmeal) {
-				for(int i=0;i<7;i++) {
-					if(i!=0)
-						Jpanel[i].setVisible(false);
-					else
-						Jpanel[i].setVisible(true);
-				}
-			}
+			if(e.getSource()==bBack) 
+				frame.dispose();
 			else if(e.getSource()==bSnack) {
-				for(int i=0;i<7;i++) {
-					if(i!=1)
-						Jpanel[i].setVisible(false);
-					else
-						Jpanel[i].setVisible(true);
-				}
+				String[] copySnack = new String[MainFrame.snack];
+				lCookList.setListData(snack);
 			}
-			else if(e.getSource()==bKorean) {
-				for(int i=0;i<7;i++) {
-					if(i!=2)
-						Jpanel[i].setVisible(false);
-					else
-						Jpanel[i].setVisible(true);
-				}
-			}
-			else if(e.getSource()==bBack) {
-				MainFrame mainFrame = new MainFrame();
-				frame.setVisible(false);
-			}
+			else if(e.getSource()==bSoup) 
+				lCookList.setListData(soup);
+			else if(e.getSource()==bSide) 
+				lCookList.setListData(side);
+			else if(e.getSource()==bRice) 
+				lCookList.setListData(rice);
+			else if(e.getSource()==bEtc) 
+				lCookList.setListData(etc);
+			else if(e.getSource()==bSearch) {
+				String searchCook = tSearch.getText(); 
+				String[] searchedCook = new String[index];
 
+				for(int i=0;i<index;i++) // cook 초기화 작업
+					cook[i]=compare[i];
+
+				int j=0;
+				for(int i=0;i<index;i++) {
+					if(cook[i].contains(searchCook)) {
+						searchedCook[j]=cook[i];
+						j++;
+					}
+				}
+				lCookList.setListData(searchedCook);
+			}
 		}
 	}
 }
